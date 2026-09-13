19dc271e6a0065375a67f60f1fd7a653ba794852
Bond007007007007 <root@localhost.localdomain>
2026-09-13 19:25:26 +0000

    Add GATE XL Topicwise Optionals bank (39 Qs): Botany/Zoology/Micro/Biochem mock questions, pool 1167->1206, tags already in index.html

---
+++ bank-xl-optionals.js
@@ -0,0 +1,784 @@
+// GATE XL Topicwise Optionals (easybiologyclass mock tests)
+window.GATE_BANK = window.GATE_BANK || {};
+window.GATE_BANK["xl"] = (window.GATE_BANK["xl"] || []).concat([
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Which of the following chromosomal aberration results in the formation of dicentric and acentric chromosomes?",
+  "options": {
+   "a": "Paracentric inversion",
+   "b": "Pericentric inversion",
+   "c": "Reciprocal translocation",
+   "d": "Deletion"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Diplospory and parthenogenesis leads to development of:",
+  "options": {
+   "a": "Haploid embryo",
+   "b": "Diploid embryo",
+   "c": "Viviparous embryo",
+   "d": "Adventive embryo"
+  },
+  "correct": [
+   "d"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Match the following and choose the correct combination: (a). Sulphur : (1). Chlorophyll (b). Zinc : (2). Nitrogenase (c). Magnesium : (3). Methionine (d). Molybdenum : (4). Auxin",
+  "options": {
+   "a": "a \u2013 1, b \u2013 2, c \u2013 3, d \u2013 4",
+   "b": "a \u2013 3, b \u2013 4, c \u2013 1, d \u2013 2",
+   "c": "a \u2013 3, b \u2013 1, c \u2013 2, d \u2013 4",
+   "d": "a \u2013 2, c \u2013 4, c \u2013 1, d \u2013 3"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "With reference to plants, gynogenesis refers to:",
+  "options": {
+   "a": "Initiation of gynoecium",
+   "b": "Development of gynoecium",
+   "c": "Development of haploid plants from the egg cell of unfertilized female gametophyte in vitro",
+   "d": "Development of an ovary"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Mangrove swamps are found along _________",
+  "options": {
+   "a": "Tropical and subtropical coast-lines",
+   "b": "Rocky coastlines",
+   "c": "Temperate coastlines",
+   "d": "Arctic coastlines"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "RAPD marker is:",
+  "options": {
+   "a": "Recessive marker",
+   "b": "Co-dominant marker",
+   "c": "Dominant marker",
+   "d": "Partially dominant marker"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "The groups which are evolved from more than one ancestor are known as:",
+  "options": {
+   "a": "Monophyletic",
+   "b": "Paraphyletic",
+   "c": "Polyphyletic",
+   "d": "Heterophyletic"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Which of the following is a natural dye obtained from plant?",
+  "options": {
+   "a": "Acid fuchsin",
+   "b": "Haematoxylin",
+   "c": "Carmine",
+   "d": "Aniline blue"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Cryopreservation is a process of:",
+  "options": {
+   "a": "Ex situ in vitro conservation",
+   "b": "Ex situ ex vitro conservation",
+   "c": "In situ in vitro conservation",
+   "d": "In situ ex vitro conservation"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "BOTANY",
+  "type": "mcq",
+  "marks": 1,
+  "q": "When oxygen reacts with rubisco enzyme the following products will be formed?",
+  "options": {
+   "a": "3-phosphoglycerate and 2-phosphoglycolate",
+   "b": "Two molecules of 3-phosphoglycerate",
+   "c": "Two molecules of 2-phosphoglycolate",
+   "d": "Glyceraldehyde 3-phosphate and 2-phosphoglycolate"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BOTANY"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Red green colour blindness is an example of _____ genetic disorder.",
+  "options": {
+   "a": "X-linked recessive",
+   "b": "X-linked dominant",
+   "c": "Y-linked recessive",
+   "d": "Y-linked dominant"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "The protein binding site in DNA can be best identified by the following experiment:",
+  "options": {
+   "a": "DNA foot printing",
+   "b": "Mobility shift assay",
+   "c": "Western blotting",
+   "d": "DNA fingerprinting"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Type of migration where marine fishes migrate to freshwater for their reproduction is:",
+  "options": {
+   "a": "Anadromus",
+   "b": "Catadromus",
+   "c": "Ocanodromus",
+   "d": "Potamodromus"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "In a hypothetical diploid animal species, there is a locus with two alleles A and B. If A and B are co-dominant, then how many different phenotypes are possible in this species?",
+  "options": {
+   "a": "1",
+   "b": "2",
+   "c": "3",
+   "d": "4"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "If a woman having blood group A with I^A I^O genotype married a man having a blood group with I^B I^B genotype, the expected proportion of heterozygotes among their children is:",
+  "options": {
+   "a": "25%",
+   "b": "50%",
+   "c": "75%",
+   "d": "100%"
+  },
+  "correct": [
+   "d"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "The propeller twist in B-form off DNA is approximately",
+  "options": {
+   "a": "+30\u25e6",
+   "b": "-30\u25e6",
+   "c": "+16\u25e6",
+   "d": "-36\u25e6"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Noble laureate Barbara McClintock observed frequent breakage of maize chromosome 9 due to which of the following transposons?",
+  "options": {
+   "a": "IS (Insertion elements)",
+   "b": "AC-Ds (Activator-Dissociator elements)",
+   "c": "Mu (Mutator)",
+   "d": "Spm/En (Suppressor-Mutator/enhancer)"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Weismann proposed the theory of:",
+  "options": {
+   "a": "Continuity of germplasm",
+   "b": "Continuity of protoplasm",
+   "c": "Continuity of plasma cells",
+   "d": "Continuity of cytoplasms"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "As part of the circadian clock, transfer of phosphate groups from adenosine triphosphate (ATP) to PER is required. This is achieved by one of the following enzyme:",
+  "options": {
+   "a": "Caesin kinase 1",
+   "b": "Caesin kinase 2",
+   "c": "Tyrosine kinase 1",
+   "d": "Tyrosine kinase 2"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "ZOO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Which one of the following is a functional excretory unit of Cephalochordata?",
+  "options": {
+   "a": "Nephridia",
+   "b": "Flame cell",
+   "c": "Solanocytes",
+   "d": "Nephron"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise ZOO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "In bacterial glucose phospho-transferase system, glucose is converted to glucose 6-phosphate. What is the source of phosphate?",
+  "options": {
+   "a": "ATP",
+   "b": "Inorganic phosphate",
+   "c": "Phosphoenol pyruvate",
+   "d": "Creatine phosphate"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Protein synthesis in eukaryotes is inhibited by:",
+  "options": {
+   "a": "Chloramphenicol",
+   "b": "Streptomycin",
+   "c": "Cephalosporin C",
+   "d": "Cycloheximide"
+  },
+  "correct": [
+   "d"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "The common feature of the following bacteriophages \u03d5X174, OB, N4 and P1 is:",
+  "options": {
+   "a": "Single stranded DNA genome",
+   "b": "RNA genome",
+   "c": "Circular genome",
+   "d": "All infect E. coli"
+  },
+  "correct": [
+   "d"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Helicobacter pylori uses urease to counteract a chemical defense in the human organ in which it lives. This chemical defense is:",
+  "options": {
+   "a": "Lysozyme",
+   "b": "Hydrochloric acid",
+   "c": "Superoxide radicals",
+   "d": "Sebum"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Which one of the following is a true combination for Kala-azar disease?",
+  "options": {
+   "a": "Sand fly and Leishmania",
+   "b": "Glossina fly and Trypanosoma",
+   "c": "Mosquito and Plasmodium",
+   "d": "Mosquito and Micro-filarial Worm"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "The type of microscopy we would use to examine bacteria that are stained by gram staining procedure is:",
+  "options": {
+   "a": "Bright field microscope",
+   "b": "Fluorescence microscopy",
+   "c": "Dark field microscopy",
+   "d": "Transmission electron microscopy"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "When bacteria growing 20oC are transferred to 40oC, they are most likely to synthesize membrane lipids with more:",
+  "options": {
+   "a": "Short chain saturated fatty acids",
+   "b": "Long chain saturated fatty acids",
+   "c": "Short chain unsaturated fatty acids",
+   "d": "Long chain unsaturated fatty acids"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Severe Combined Immunodeficiency (SCID) mice do not have:",
+  "options": {
+   "a": "Eosinophils",
+   "b": "Basophils",
+   "c": "Neutrophils",
+   "d": "B &amp; T cells"
+  },
+  "correct": [
+   "d"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Which of the following technique is best for determining the precise location of the radioactive isotope located in a specimen?",
+  "options": {
+   "a": "Ultracentrifugation",
+   "b": "Atomic force microscope",
+   "c": "Autoradiography",
+   "d": "Fluorescence microscope"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "MICRO",
+  "type": "mcq",
+  "marks": 1,
+  "q": "__________ are the simplest transposable element that contain inverted repeat sequences at each end and a gene encoding transposase.",
+  "options": {
+   "a": "Composite transposon",
+   "b": "Insertion element",
+   "c": "Virus",
+   "d": "Prion"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise MICRO"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "How many different linear tripeptides can be made from three different L - \u03b1 neutral amino acids, using each amino acid only once in the chain?",
+  "options": {
+   "a": "3",
+   "b": "6",
+   "c": "12",
+   "d": "27"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "In anaerobic glycolysis, 2 molecules of inorganic phosphate are used per molecule of glucose consumed. Which of the following enzymes catalyzes the uptake of inorganic phosphate?",
+  "options": {
+   "a": "Hexokinase",
+   "b": "Phosphofructokinase",
+   "c": "Glyceraldehyde 3-phosphate dehydrogenase",
+   "d": "Pyruvate kinase"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Mark the correct statement:",
+  "options": {
+   "a": "In active transport, molecules move from higher to lower concentration",
+   "b": "Carrier protein is involved in both active transport and facilitated diffusion",
+   "c": "Energy is consumed to move molecules against concentration gradient in active transport only",
+   "d": "In active transport, only water molecules are transported with consuming energy"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Which of the following is not a method of enzyme immobilization?",
+  "options": {
+   "a": "Entrapment",
+   "b": "Adsorption",
+   "c": "Absorption",
+   "d": "Cross-linking"
+  },
+  "correct": [
+   "c"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "The enzyme Aacetyl Co-A carboxylase belongs to which class of enzyme?",
+  "options": {
+   "a": "Transferases",
+   "b": "Hydrolases",
+   "c": "Lyses",
+   "d": "Ligases"
+  },
+  "correct": [
+   "d"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "In non-competitive inhibition:",
+  "options": {
+   "a": "The inhibitor binds to the active site of enzyme",
+   "b": "The inhibitor binds at a site other than active site and may bind to either E or ES complex",
+   "c": "The inhibitor binds to a site other than active site and binds only to the ES complex",
+   "d": "The inhibitor binds at a site other than active site and binds only to E"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "When the velocity of enzyme activity is plotted against substrate concentration, which of the following is obtained?",
+  "options": {
+   "a": "Hyperbolic curve",
+   "b": "Parabola",
+   "c": "Straight line with positive slope",
+   "d": "Straight line with negative slope"
+  },
+  "correct": [
+   "a"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "NMR is a very important technique for determination of biopolymer structures. NMR experiments are not limited to the study of protons only. Which of the following represents a correct group of isotopes used to obtain resonance signals in NMR?",
+  "options": {
+   "a": "1H, 12C, 15N, 19F",
+   "b": "1H, 13C, 14N, 19F",
+   "c": "1H, 12C, 14N, 19F",
+   "d": "1H, 13C, 15N, 19F"
+  },
+  "correct": [
+   "d"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ },
+ {
+  "section": "BIOCHEM",
+  "type": "mcq",
+  "marks": 1,
+  "q": "Number of nuclear pores on the nuclear membrane depend on the:",
+  "options": {
+   "a": "Surface volume of nuclear membrane",
+   "b": "Transcription activity of the cell",
+   "c": "DNA content of the cell",
+   "d": "Size of the nucleus"
+  },
+  "correct": [
+   "b"
+  ],
+  "ans": null,
+  "tol": null,
+  "explain": "",
+  "topic": "",
+  "src": "XL Topicwise BIOCHEM"
+ }
+]);
